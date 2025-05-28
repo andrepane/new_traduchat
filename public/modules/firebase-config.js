1f13fba50260c788a2315bba0c0dd5f8d5e508d3
andrepane <156331723+andrepane@users.noreply.github.com>
2025-05-28 09:57:50 +0200

    Create firebase-config.js

---
+++ public/modules/firebase-config.js
@@ -0,0 +1,10 @@
+// public/modules/firebase-config.js
+
+export const firebaseConfig = {
+    apiKey: "tu-api-key",
+    authDomain: "tu-auth-domain",
+    projectId: "tu-project-id",
+    storageBucket: "tu-storage-bucket",
+    messagingSenderId: "tu-messaging-sender-id",
+    appId: "tu-app-id"
+};
