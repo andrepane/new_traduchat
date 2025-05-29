5d4476a5cc06b86d8d858634ba63454c7fdbbaf4
andrepane <156331723+andrepane@users.noreply.github.com>
2025-05-29 11:08:57 +0200

    Update firebase-config.js

--- public/modules/firebase-config.js
+++ public/modules/firebase-config.js
@@ -1,10 +1,12 @@
 // public/modules/firebase-config.js
 
+// public/modules/firebase-config.js
 export const firebaseConfig = {
-    apiKey: "tu-api-key",
-    authDomain: "tu-auth-domain",
-    projectId: "tu-project-id",
-    storageBucket: "tu-storage-bucket",
-    messagingSenderId: "tu-messaging-sender-id",
-    appId: "tu-app-id"
+    apiKey: "AIzaSyBPurWNRib5yjg-jEe3x2hBewL_Cvy132E",
+    authDomain: "traduchat-2.firebaseapp.com",
+    databaseURL: "https://traduchat-2-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "traduchat-2",
+    storageBucket: "traduchat-2.firebasestorage.app",
+    messagingSenderId: "304746474467",
+    appId: "1:304746474467:web:a0496a8d1d891cec170ed6"
 };
