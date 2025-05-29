d70bbe95334352b2bf3e123b6596deff6c1097b9
andrepane <156331723+andrepane@users.noreply.github.com>
2025-05-29 08:19:09 +0200

    Delete public/modules/ui.js

--- public/modules/ui.js
+++
@@ -1,8 +0,0 @@
-import { translateInterface } from './translations.js';
-import { getUserLanguage } from './state.js';
-
-export function updateUITranslations() {
-    const lang = getUserLanguage();
-    console.log("🌐 updateUITranslations ejecutado con idioma:", lang);
-    translateInterface(lang);
-}
