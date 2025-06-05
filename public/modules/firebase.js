8c433f2a2b7b604812f62c83d4fbaae914b55d72
andrepane <156331723+andrepane@users.noreply.github.com>
2025-06-05 11:31:12 +0200

    Update firebase.js

--- public/modules/firebase.js
+++ public/modules/firebase.js
@@ -3,7 +3,7 @@ import { initializeApp, getApp, getApps } from 'https://www.gstatic.com/firebase
 
 import { getAuth, setPersistence, browserLocalPersistence } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js';
-import { getMessaging, isSupported } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging.js';
+import { getMessaging } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging.js';
 import { getStorage } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js';
 
 import { firebaseConfig } from './firebase-config.js'; // ← usa tu archivo actual
@@ -21,6 +21,6 @@ try {
     }
 } catch (err) {
     console.warn('Firebase Messaging no soportado:', err);
-}const storage = getStorage(app);
+const storage = getStorage(app);
 
 export { app, auth, db, messaging, storage };
